4da9615412f20439f700fad10ec35c82eea3f81b
hiroi9361 <hiroi9361@gmail.com>
2025-04-19 18:20:40 +0900

    　images/,kadai6_2.md

--- kadai1/src/kadai6/GetNum.java
+++ kadai1/src/kadai6/GetNum.java
@@ -6,27 +6,30 @@ import java.util.Scanner;
 
 public class GetNum {
 	public static List<Integer> getNum() {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		List<Integer> numbers = new ArrayList<>();
 		int maxInput = 5;
-		
-		System.out.println(maxInput + "回以下の整数を入力してください。");
-		
+
+		System.out.println(maxInput + "回以下の整数を入力してください。excute と入力すると実行します。");
+
 		int count = 0;
-		while(count < maxInput && scanner.hasNextInt()) {
-			numbers.add(scanner.nextInt());
-			count++;
-		}
-		
-		if(count > maxInput && scanner.hasNextInt()) {
-			System.out.println("入力回数が上限の" + maxInput + "回に達しました。");
-			
-			while(scanner.hasNextInt()) {
-				scanner.nextInt(); // 読み飛ばす
+		while(count < maxInput && scanner.hasNext()) {
+			String input = scanner.next();
+
+			if(input.equalsIgnoreCase("excute")) {
+				System.out.println("入力ありがとうございます。");
+				break;
+			}
+
+			try {
+				int num = Integer.parseInt(input);
+				numbers.add(num);
+				count++;
+			} catch(NumberFormatException e) {
+				System.out.println("無効な入力です。");
 			}
 		}
-		
 		return numbers;
 	}
 }
